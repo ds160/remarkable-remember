d3c5d80855a89d53518848d69e5ca41459f4a081
Daniel Schneider <SchneiderD@gmx.net>
2023-12-27 21:43:01 +0100

    Adds first template images

--- src/ViewModels/TemplateIconCodeViewModel.cs
+++ src/ViewModels/TemplateIconCodeViewModel.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace ReMarkableRemember.ViewModels;
 
 public sealed class TemplateIconCodeViewModel
 {
-    private TemplateIconCodeViewModel(String code, String name)
+    private static readonly String? assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+    private TemplateIconCodeViewModel(String code, String name, String image)
     {
         this.Code = code;
+        this.Image = new Bitmap(AssetLoader.Open(new Uri($"avares://{assemblyName}/Assets/Templates/{image}.png")));
         this.Name = name;
     }
 
     public String Code { get; }
+
+    public Bitmap Image { get; }
 
     public String Name { get; }
 
@@ -19,10 +27,10 @@ public sealed class TemplateIconCodeViewModel
     {
         return new List<TemplateIconCodeViewModel>()
         {
-            new TemplateIconCodeViewModel("\uE9FE", "Blank"),
-            new TemplateIconCodeViewModel("\uE98F", "Checklist"),
-            new TemplateIconCodeViewModel("\uEA00", "Isometric"),
-            new TemplateIconCodeViewModel("\uE9A8", "Lined small"),
+            new TemplateIconCodeViewModel("\uE9FE", "Blank", "E9FE"),
+            new TemplateIconCodeViewModel("\uE98F", "Checklist", "E98F"),
+            new TemplateIconCodeViewModel("\uEA00", "Isometric", "EA00"),
+            new TemplateIconCodeViewModel("\uE9A8", "Lined small", "E9A8"),
         };
     }
 }
