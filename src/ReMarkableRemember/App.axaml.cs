f6d5c3ab57f8ac9ae66ddef14d7098a39206c788
Daniel Schneider <daniel@schneider.dev>
2026-03-11 01:46:46 +0100

    Deleted DependencyManager

--- src/ReMarkableRemember/App.axaml.cs
+++ src/ReMarkableRemember/App.axaml.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using ReMarkableRemember.Services.ConfigurationService;
+using ReMarkableRemember.Services.DataService;
+using ReMarkableRemember.Services.HandWritingRecognitionService;
+using ReMarkableRemember.Services.TabletService;
 using ReMarkableRemember.ViewModels;
 using ReMarkableRemember.Views;
 
@@ -18,12 +24,24 @@ public partial class App : Application
     {
         if (this.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopApp)
         {
-            DependencyManager dependencyManager = new DependencyManager(desktopApp.Args);
-            Object dataContext = dependencyManager.Resolve<MainWindowModel>();
+            Object dataContext = CreateDataContext(desktopApp.Args);
             desktopApp.MainWindow = new MainWindow() { DataContext = dataContext };
             this.DataContext = dataContext;
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static MainWindowModel CreateDataContext(String[]? args)
+    {
+        IServiceProvider serviceProvider = new ServiceCollection()
+            .AddSingleton<IConfigurationService, ConfigurationServiceDataService>()
+            .AddSingleton<IDataService>(DataServiceSqlite.Create(args?.FirstOrDefault()))
+            .AddSingleton<IHandWritingRecognitionService, HandWritingRecognitionServiceMyScript>()
+            .AddSingleton<ITabletService, TabletService>()
+            .AddSingleton<MainWindowModel>()
+            .BuildServiceProvider();
+
+        return serviceProvider.GetRequiredService<MainWindowModel>();
     }
 }
